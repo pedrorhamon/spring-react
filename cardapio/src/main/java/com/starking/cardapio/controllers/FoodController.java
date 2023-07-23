c190d4a6ec219c92cf001d6b3ff2a7fad365c2dc
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-07-23 19:24:38 -0300

    criando um metodo no controller

--- cardapio/src/main/java/com/starking/cardapio/controllers/FoodController.java
+++ cardapio/src/main/java/com/starking/cardapio/controllers/FoodController.java
@@ -2,10 +2,15 @@ package com.starking.cardapio.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.starking.cardapio.domain.dtos.FoodRequestDTO;
 import com.starking.cardapio.domain.dtos.FoodResponseDTO;
 import com.starking.cardapio.services.FoodService;
 
@@ -21,6 +26,12 @@ public class FoodController {
 
 	private final FoodService foodService;
 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void saveFood(@RequestBody FoodRequestDTO data) {
+		this.foodService.save(data);
+	}
+	
 	@GetMapping
 	public List<FoodResponseDTO> getAll() {
 		return this.foodService.getAll();
