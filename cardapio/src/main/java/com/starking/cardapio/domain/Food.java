fde924582414a8a5aa819dc96fbeb35ccf310869
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-07-23 13:29:43 -0300

    criando atributos na entidade

--- cardapio/src/main/java/com/starking/cardapio/domain/Food.java
+++ cardapio/src/main/java/com/starking/cardapio/domain/Food.java
@@ -1,9 +1,29 @@
 package com.starking.cardapio.domain;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /**
  * @author pedroRhamon
  */
 
-public class Food {
+@Entity(name = "foods")
+@Table(name = "foods")
+public class Food implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String title;
 
 }
