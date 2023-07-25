7c5b1974f15dce896be3d3c341a88aedf9b43c04
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-07-25 19:43:55 -0300

    criando modal para aplicação

--- cardapio/src/hooks/useFoodData.ts
+++ cardapio/src/hooks/useFoodData.ts
@@ -1,22 +1,22 @@
-import axios, {AxiosPromise} from 'axios';
-import {useQuery} from "@tanstack/react-query"
-import { FoodData } from '../components/card/interface/FoodData';
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosPromise } from "axios"
+import { FoodData } from '../interface/FoodData';
 
 const API_URL = 'http://localhost:8080';
 
-const  fetchData = async (): AxiosPromise<FoodData[]> => {
+const fetchData = async (): AxiosPromise<FoodData[]> => {
     const response = axios.get(API_URL + '/food');
     return response;
-} 
+}
 
-export function useFoodData() {
+export function useFoodData(){
     const query = useQuery({
         queryFn: fetchData,
         queryKey: ['food-data'],
         retry: 2
     })
 
-    return{
+    return {
         ...query,
         data: query.data?.data
     }
